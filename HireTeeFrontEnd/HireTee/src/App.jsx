38533acf464c0a60d7c727295a28e61e4e8d621b
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-11-27 22:36:37 -0800

    FAQ Initialized

--- HireTeeFrontEnd/HireTee/src/App.jsx
+++ HireTeeFrontEnd/HireTee/src/App.jsx
@@ -1,16 +1,14 @@
-
-
-import './App.css'
+import React from 'react';
+import FAQ from './components/FAQ'; // Adjust the path to point to the FAQ.jsx file
 
 function App() {
-
-  return (
-    <>
-      <div>
-          App
-      </div>
-    </>
-  )
+    return (
+        <div>
+            <h1>Welcome to HireTee</h1>
+            {/* Call the FAQ component */}
+            <FAQ />
+        </div>
+    );
 }
 
-export default App
+export default App;
